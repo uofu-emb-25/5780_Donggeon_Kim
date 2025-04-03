3b157dd65f59c4b6391245756200c25812c42388
Donggeon Kim <jsks12312@gmail.com>
2025-04-02 19:56:49 -0600

    lab7 motor output to be set

--- Src/lab7.c
+++ Src/lab7.c
@@ -1,9 +1,30 @@
+#include "stm32f0xx.h"
 #include "lab7.h"
 
-void lab7_checkoff(void) {
-    // Placeholder for future initialization
+#define MOTOR_EN_Pin   GPIO_PIN_8
+#define MOTOR_DIRA_Pin GPIO_PIN_9
+#define MOTOR_DIRB_Pin GPIO_PIN_10
+#define MOTOR_PORT     GPIOC
+
+void lab7_Init(void) {
+    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+
+    // Set pins to output
+    MOTOR_PORT->MODER |= (1 << (2 * 8))| (1 << (2 * 9)) | (1 << (2 * 10));
 }
 
-void lab7_SysTick_Handler(void) {
-    // Placeholder for SysTick functionality
+void Set_Motor_Output(int enable, int direction) {
+    if (enable) {
+        MOTOR_PORT->ODR |= MOTOR_EN_Pin;
+    } else {
+        MOTOR_PORT->ODR &= ~MOTOR_EN_Pin;
+    }
+
+    if (direction >= 0) {
+        MOTOR_PORT->ODR |= MOTOR_DIRA_Pin;
+        MOTOR_PORT->ODR &= ~MOTOR_DIRB_Pin;
+    } else {
+        MOTOR_PORT->ODR &= ~MOTOR_DIRA_Pin;
+        MOTOR_PORT->ODR |= MOTOR_DIRB_Pin;
+    }
 }
